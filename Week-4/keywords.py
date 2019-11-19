011e3ceb3398fd0844c812ac7f94e68b782eab38
LegoYoda112 <thomas.godden@outlook.com>
2019-11-19 11:00:23 +0000

    Things I forgot to commit at some point

--- Week-4/keywords.py
+++ Week-4/keywords.py
@@ -14,7 +14,17 @@ with default values.
 # it returns 'Hello and welcome to <place>'
 # if both user_name and place are provided
 # it returns 'Hello, <user_name>, and welcome to <place>
+def welcome_message(user_name = None, place = None):
+    name_string = ''
+    if user_name != None:
+        name_string = ', ' + user_name + ','
 
+    place_string = ''
+    if place != None:
+        place_string = ' to ' + place
+
+    return_string = 'Hello' + name_string + ' and welcome' + place_string
+    return return_string
 
 # Create a function called list_average()
 # without using any libraries to do the maths for you 
@@ -27,3 +37,48 @@ with default values.
 # (return list of all modes if there is a tie between multiple values)
 # if avg_type='mean', return the mean of the list
 # if avg_type='median', return the median of this list
+def unique(input_list):
+    unique_list = []
+    for number in input_list:
+        if number not in unique_list:
+            unique_list.append(number)
+    return unique_list
+
+def secondTuple(input_tuple):
+    return input_tuple[1]
+
+def list_average(list, avg_type = 'mean'):
+
+    if avg_type == 'mode':
+        counted_list = []
+
+        unique_list = unique(list)
+
+        for number in unique_list:
+            counted_list.append((number, list.count(number)))
+        
+        counted_list = sorted(counted_list, key = secondTuple, reverse = True)
+        
+        return_list = []
+        max_count = counted_list[0][1]
+        print(max_count)
+
+        for counted_pair in counted_list:
+            number, count = counted_pair
+
+            if(count < max_count):
+                break 
+
+            return_list.append(number)
+            
+        return return_list
+
+    if avg_type == 'mean':
+        list_sum = 0.0
+        for number in list:
+            list_sum += number
+        return list_sum/len(list)
+
+    if avg_type == "median":
+        sorted_list = sorted(list)
+        return sorted_list[int(len(list)/2)]
